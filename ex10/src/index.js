818fe73e150386a809501505d9ce18c37bcdf38c
Kemal Arifovic <gigalab@gmail.com>
2021-03-11 12:01:22 +0100

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  // Only change code below this line
+  var myFirstName = "Kemal";
+  var myLastName = "Arifović";
+  var myHomeTown = "Zenica";
+  // Only change code above this line
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
